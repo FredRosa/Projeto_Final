ca51cdbddf1c4d01f0d2871e2fffa8d0fc7c6482
Frederico <7.frederico@gmail.com>
2021-08-19 17:34:31 +0100

    TABELAS SQL CSS recover

--- php/clientes.php
+++ php/clientes.php
@@ -1,14 +1,47 @@
 <html>
     <head>
-        <link href="../css/styles.css" rel="stylesheet" />
+
+    <style type="text/css">
+        table {
+        margin: 8px;
+        }
+
+        th {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: .7em;
+        background: #658062;
+        color: #FFF;
+        padding: 2px 6px;
+        border-collapse: separate;
+        border: 1px solid #000;
+        }
+
+        td {
+          font-family: Arial, Helvetica, sans-serif;
+          font-size: 1em;
+          border: 1px solid;
+          color: #658062;
+
+        }
+
+        button {
+          color: white;
+          background-color: #658062;
+          border-color: #658062;
+          margin-left: 20px;
+          margin-right: 20px;
+          margin-top: 17px;
+        }
+
+        button:hover {
+          color: rgb(255, 255, 255);
+          background-color: #55BF49;;
+          border-color: #55BF49;;
+        }
+
+</style>
+
     </head>
-    <body>
-
-        <button onclick="document.location='../admin2.html'">voltar</button>
-
-        <br>
-
-    </body>
 
 </html>
 
@@ -20,9 +53,9 @@ $sql = "SELECT * FROM clientes";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-  echo "<table style='background-color:white'><tr><th>nome</th><th>email</th><th>contacto</th><th>morada</th><th>newsletter</th></tr>";
+  echo "<table><tr><th>nome</th><th>email</th><th>contacto</th><th>morada</th><th>newsletter</th></tr>";
   while($row = $result->fetch_assoc()) {
-    echo "<tr><td id='tabela_admin'>".$row["nome"] ."</td> <td>" . $row["email"] . "</td> <td>" . $row["contacto"] . "</td> <td>". $row["morada"] . "</td> <td>". $row["newsletter"] . "</td><td>
+    echo "<tr style='padding:5px; border-style:solid'><td>".$row["nome"] ."</td> <td>" . $row["email"] . "</td> <td>" . $row["contacto"] . "</td> <td>". $row["morada"] . "</td> <td>". $row["newsletter"] . "</td><td>
     <form action='delete.php' method='post'>
 
     <input name='email' value='" . $row["email"] ."' hidden>
@@ -32,10 +65,12 @@ if ($result->num_rows > 0) {
 </form>
     </tr>";
   }
-  echo "</table>";
+  echo "</table> <br> <button onclick=document.location='../admin2.html'>voltar</button>";
+  
 } else {
   echo "Sem resultados";
 }
+
 $conn->close();
 
 ?>
--- php/especialidades.php
+++ php/especialidades.php
@@ -1,14 +1,45 @@
 <html>
     <head>
-        <link href="../css/styles.css" rel="stylesheet"/>
+    <style type="text/css">
+        table {
+        margin: 8px;
+        }
+
+        th {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: .7em;
+        background: #658062;
+        color: #FFF;
+        padding: 2px 6px;
+        border-collapse: separate;
+        border: 1px solid #000;
+        }
+
+        td {
+          font-family: Arial, Helvetica, sans-serif;
+          font-size: 1em;
+          border: 1px solid;
+          color: #658062;
+
+        }
+
+        button {
+          color: white;
+          background-color: #658062;
+          border-color: #658062;
+          margin-left: 20px;
+          margin-right: 20px;
+          margin-top: 17px;
+        }
+
+        button:hover {
+          color: rgb(255, 255, 255);
+          background-color: #55BF49;;
+          border-color: #55BF49;;
+        }
+
+</style>
     </head>
-    <body>
-
-        <button onclick="document.location='../admin2.html'">Voltar</button>
-
-        <br>
-
-    </body>
 
 </html>
 
@@ -24,7 +55,7 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
     echo "<tr><td>".$row["ementa"]."</td><td>".$row["preço"] . "</td></tr>";
   }
-  echo "</table>";
+  echo "</table> <br> <button onclick=document.location='../admin2.html'>voltar</button>";
 } else {
   echo "Sem resultados";
 }
