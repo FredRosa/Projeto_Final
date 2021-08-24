444552a2c1572eba05f362732716b83bee07f0c1
Inês <inespenas@hotmail.com>
2021-08-24 21:14:42 +0100

    alteraçao de links para index php

--- php/create.php
+++ php/create.php
@@ -13,7 +13,7 @@ $newsletter = $_POST['newsletter'];
 
 $query = "INSERT INTO clientes(nome,email,pass,contacto,morada,newsletter) VALUES ('$nome','$email', '$password','$contacto', '$morada','$newsletter')";
 if (mysqli_query($conn, $query)){
-    header('Location: '.$uri.'../index.html');
+    header('Location: '.$uri.'../index.php');
 }
 else{
     echo "Error Inserting record: " . $conn->error;
